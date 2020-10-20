5975ff46b5a4333ca9a25baa640d46259898d9f6
Arianaomi <arianaomi.lp@gmail.com>
2020-10-20 02:31:41 -0500

    update path

---
+++ stories/InfoCard.stories.tsx
@@ -0,0 +1,32 @@
+
+import React from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import InfoCard, { Props } from '../components/BootcampCard'
+
+export default {
+  title: 'Base/InfoCard',
+  component: InfoCard,
+  argTypes: {
+    icon: {
+      control: 'text'
+    },
+    title: {
+      control: 'text'
+    },
+    description: {
+      control: 'text'
+    }
+  }
+
+} as Meta
+
+const Template: Story<Props> = (args) => <InfoCard {...args} />
+
+export const Info = Template.bind({})
+Info.args = {
+  icon: '/icons/Icon-Objetivo.svg',
+  title: 'Te acompañamos en el proceso',
+  description: 'Todos los días, por módulos y por Bootcamp, llevarás proyectos prácticos reales'
+}
