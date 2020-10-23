135561a2607cfb9c358ed848717917d79e93513a
arianaomi <arianaomi.lp@gmail.com>
2020-10-23 15:09:01 -0500

    ADD h2-white-cursor and h2-cyan-cursor components

--- components/H1.tsx
+++ components/H1.tsx
@@ -1,18 +1,16 @@
 
 import React from 'react'
 //my component
-import Cursor from './TextCursor'
+import TextCursor from './TextCursor'
 
 export interface Props {
   text: string
 }
 
 export default function H1 ({ text }: Props) {
-  
-
   return (
     <h1 className ='h1'>
-      <Cursor h1 text= {text} white/>
+      <TextCursor text= {text} white/>
     </h1>
   )
 };
