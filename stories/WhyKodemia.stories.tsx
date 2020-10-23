e585e9c5ed16029f5f59c339d802592cbc6a4a41
rrortegaa <rrortegaa@gmail.com>
2020-10-23 01:03:05 -0500

    add story for this component

---
+++ stories/WhyKodemia.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+
+
+import WhyKodemia from '../components/WhyKodemia'
+
+export default {
+  title: 'base/WhyKodemia',
+  component: WhyKodemia
+} as Meta
+
+const Template: Story = () => <WhyKodemia />
+
+export const Default = Template.bind({})
