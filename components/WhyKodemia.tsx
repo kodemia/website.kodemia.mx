53375d8b9090118c987fb032fe9985d86472c95f
rrortegaa <rrortegaa@gmail.com>
2020-10-23 02:37:40 -0500

    add new features for WhyKodemia component

--- components/WhyKodemia.tsx
+++ components/WhyKodemia.tsx
@@ -1,18 +1,28 @@
-import InfoCard from './InfoCard'
 
-export default function WhyKodemia () {
+import InfoCard, { Props as InfoCardProps } from './InfoCard'
+
+export interface Props {
+  cards: Array<InfoCardProps>
+}
+
+export default function WhyKodemia ({ cards }: Props) {
   return (
     <section className='why-kodemia'>
       <div className='container'>
         <h3 className='subtitle'>¿Por qué Kodemia?</h3>
         <h2 className='title'>Vivimos para <span>formar programadores</span></h2>
         <img className='image' src='/images/why-kodemia.png' alt='koder y mentor sonriendo' />
-        <div className='cards'>
-          <InfoCard
-            icon=''
-            title=''
-            description=''
-          />
+        <div className='why-kodemia-cards'>
+          {cards.map((card, index) => {
+            return (
+              <InfoCard
+                key={`card-${index}`}
+                icon={card.icon}
+                title={card.title}
+                description={card.description}
+              />
+            )
+          })}
         </div>
       </div>
     </section>
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,29 @@ import InfoCard from '../components/InfoCard'
 import Navbar from '../components/Navbar'
 import WhyKodemia from '../components/WhyKodemia'
 
+const cards = [
+  {
+    description: 'Queremos ser parte de tu nuevo futuro como desarrollador desde el día uno, hasta después del Bootcamp',
+    icon: '/icons/Icon-Proceso.svg',
+    title: 'Te acompañamos en el proceso'
+  },
+  {
+    description: 'Nos adaptamos a lo que estás buscando, ya sea colocarte, emprender o crecer profesionalmente',
+    icon: '/icons/Icon-Objetivo.svg',
+    title: 'Tu objetivo es nuestro objetivo'
+  },
+  {
+    description: 'Lo más importante para nosotros son las personas. Ayudarlas a conectar entre sí es parte de nuestro propósito, para que nunca dejen de crecer',
+    icon: '/icons/Icon-Comunidad.svg',
+    title: 'Creamos comunidad'
+  },
+  {
+    description: 'Nuestros egresados tienen las herramientas para enfrentarse y triunfar en el mundo laboral ',
+    icon: '/icons/Icon-Alto-calibre.svg',
+    title: 'Programadores de alto calibre'
+  }
+]
+
 export default function Home () {
   return (
     <div>
@@ -18,7 +41,7 @@ export default function Home () {
         title='Te acompañamos en el proceso'
         description='Todos los días, por módulos y por Bootcamp, llevarás proyectos prácticos reales'
       />
-      <WhyKodemia />
+      <WhyKodemia cards={cards} />
       {/* <BootcampCard
         mode='Modo Lifetime'
         date='19 de Octubre'
