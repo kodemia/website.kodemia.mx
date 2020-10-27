efacb144cfd63218dfffbfbf10a315d40155ed6b
arianaomi <arianaomi.lp@gmail.com>
2020-10-26 21:08:45 -0600

    working

---
+++ components/OurBootcamps.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+// My components
+import BootcampCard from './BootcampCard'
+import Lenguage from './LenguageCard'
+// Data
+import Bootcamps from '../config/bootcamps.json'
+
+export default function OurBootcamps () {
+  return(
+    <div className='our-bootcamps'>
+      
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import InfoCard from '../components/InfoCard'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import BootcampCard from '../components/BootcampCard'
-import LenguageCard from '../components/LenguageCard'
+import OurBootcamps from '../components/OurBootcamps'
 
 export default function Home () {
   return (
@@ -12,24 +10,8 @@ export default function Home () {
         <title>Kodemia</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <InfoCard
-        icon='/icons/Icon-Objetivo.svg'
-        title='Te acompañamos en el proceso'
-        description='Todos los días, por módulos y por Bootcamp, llevarás proyectos prácticos reales'
-      />
       <Navbar />
-      <BootcampCard
-        mode='Modo Lifetime'
-        date='19 de Octubre'
-        schedule='Lunes a viernes de 07:00 - 10:00 pm'
-        feature='Acceso de por vida a Kodemia'
-      />
-      <LenguageCard
-        name='Bootcamp FullStack con' 
-        lenguage='Javascript' 
-        description='Desarrollarás una web app completa con MERN orientado al Front End con React' 
-        duration='19em'
-      /> 
+      <OurBootcamps />
       <Footer />
     </div>
   )
