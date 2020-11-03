14fb6dce44166664e3ba72bfdd61602ffc4bb579
Arianaomi <arianaomi.lp@gmail.com>
2020-11-02 23:58:44 -0600

    ADD style kodemia-financing section

--- components/Pages/Home/KodemiaFinancing.tsx
+++ components/Pages/Home/KodemiaFinancing.tsx
@@ -2,18 +2,25 @@
 import React from 'react'
 // My component
 import Button from '../../Button'
+import H2 from '../../H2'
 import H3 from '../../H3'
 
 export default function KodemiaFinancing () {
   const data = {
-    title: 'Queremos que alcances tus metas',
+    title: 'Queremos que alcances ',
+    keywords: ' tus metas',
     section: 'Apoyo Kodemia',
-    text: 'Si tus ganas de desarrollarte superan tu capacidad de pagp, en Kodemia tenemos opciones, nos interesa escucharte.'
+    text: 'Si tus ganas de desarrollarte superan tu capacidad de pago, en Kodemia tenemos opciones, nos interesa escucharte.'
   }
+
   return (
-    <div>
-      <H3 text={data.section} />
-      <Button />
+    <div className='kodemia-financing'>
+      <div className='container-card'>
+        <H3 text={data.section} />
+        <H2 whiteText={data.title} cyanText={data.keywords} />
+        <p className='text'>{data.text}</p>
+        <Button isPrimary label='Conoce más' />
+      </div>
     </div>
   )
 }
