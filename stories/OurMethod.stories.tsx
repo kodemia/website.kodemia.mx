4c1590b14928385462c54eb326655ce5b6d02d52
Arianaomi <arianaomi.lp@gmail.com>
2020-11-05 19:41:58 -0600

    ADD our method story

---
+++ stories/OurMethod.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import OurMethod, { Props } from "../components/Pages/Home/OurMethod";
+
+export default {
+  title: "Home/OurMethod",
+  component: OurMethod,
+} as Meta;
+
+const Template: Story<Props> = (args) => <OurMethod {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  ourMethod : {
+    title: "Aprender a programar",
+    keywords: "no es fácil",
+    text:
+      'En kodemia no solo buscamso que "copies y pegues código" somo enemigos del "si funciona no le muevas " nuestro "Método" te exigirá y te llevará al limite, pero al final la recompensa es enorme',
+    cards: [
+      {
+        title: "De la teoría a la \npráctica",
+        text:
+          "Todos kis días, por módulos y por Bootcamp, llevrás proyectos prácticos reales",
+        icon: "/icons/icon-practica.svg",
+      },
+    ],
+  },
+};
