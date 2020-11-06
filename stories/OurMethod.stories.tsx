e5192f8f0cfdd0ff6949846a994a1861111e5054
Arianaomi <arianaomi.lp@gmail.com>
2020-11-05 19:42:15 -0600

    ADD our method story

--- stories/OurMethod.stories.tsx
+++ stories/OurMethod.stories.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { Story, Meta } from "@storybook/react";
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
 
-import OurMethod, { Props } from "../components/Pages/Home/OurMethod";
+import OurMethod, { Props } from '../components/Pages/Home/OurMethod'
 
 export default {
-  title: "Home/OurMethod",
-  component: OurMethod,
-} as Meta;
+  title: 'Home/OurMethod',
+  component: OurMethod
+} as Meta
 
-const Template: Story<Props> = (args) => <OurMethod {...args} />;
+const Template: Story<Props> = (args) => <OurMethod {...args} />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
-  ourMethod : {
-    title: "Aprender a programar",
-    keywords: "no es fácil",
+  ourMethod: {
+    title: 'Aprender a programar',
+    keywords: 'no es fácil',
     text:
       'En kodemia no solo buscamso que "copies y pegues código" somo enemigos del "si funciona no le muevas " nuestro "Método" te exigirá y te llevará al limite, pero al final la recompensa es enorme',
     cards: [
       {
-        title: "De la teoría a la \npráctica",
+        title: 'De la teoría a la \npráctica',
         text:
-          "Todos kis días, por módulos y por Bootcamp, llevrás proyectos prácticos reales",
-        icon: "/icons/icon-practica.svg",
-      },
-    ],
-  },
-};
+          'Todos kis días, por módulos y por Bootcamp, llevrás proyectos prácticos reales',
+        icon: '/icons/icon-practica.svg'
+      }
+    ]
+  }
+}
