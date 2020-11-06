e8f1e9d08883edd7fbd9074a74313e017083fb19
Arianaomi <arianaomi.lp@gmail.com>
2020-11-05 20:09:46 -0600

    :art: update backgroun color

--- components/Pages/Home/OurMethod.tsx
+++ components/Pages/Home/OurMethod.tsx
@@ -24,25 +24,25 @@ export interface Props {
 export default function OurMethod ({ ourMethod: { title, keywords, text, cards } }: Props) {
   return (
     <section className='our-method '>
-     <div className='our-method-container section-container'>
-     <H3 text='Nuestro Método'/>
-      <div className='title-contain'>
-        <H2 whiteText={title} cyanText={keywords} />
+      <div className='our-method-container section-container'>
+        <H3 text='Nuestro Método' />
+        <div className='title-contain'>
+          <H2 whiteText={title} cyanText={keywords} />
+        </div>
+        <p className='our-method-text'>{text}</p>
+        <div className='our-method-cards'>
+          {cards.map((infoCard, index) => {
+            return (
+              <InfoCard
+                key={`om-card-${index}`}
+                icon={infoCard.icon}
+                title={infoCard.title}
+                text={infoCard.text}
+              />
+            )
+          })}
+        </div>
       </div>
-      <p className='our-method-text'>{text}</p>
-      <div className='our-method-cards'>
-        {cards.map((infoCard, index) => {
-          return (
-            <InfoCard
-              key={`om-card-${index}`}
-              icon={infoCard.icon}
-              title={infoCard.title}
-              text={infoCard.text}
-            />
-          )
-        })}
-      </div>
-     </div>
     </section>
   )
 }
