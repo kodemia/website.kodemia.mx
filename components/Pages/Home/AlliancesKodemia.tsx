a2376151d49fd7818fcbbab8a9df7c251477e0d7
Arianaomi <arianaomi.lp@gmail.com>
2020-11-17 23:27:30 -0600

    :art: Full alliances-kodemia component

--- components/Pages/Home/AlliancesKodemia.tsx
+++ components/Pages/Home/AlliancesKodemia.tsx
@@ -6,28 +6,29 @@ import H3 from '../../H3'
 
 export interface Allies {
   name: string
-  image:string
+  image: string
+  alt: string
 }
 export interface Props {
   allies: Array<Allies>
 }
 
-export default function AllianceKodemia ({ allies }:Props) {
+export default function AllianceKodemia({ allies }: Props) {
   return (
     <section className='alliances-kodemia'>
       <div className='alliances-kodemia-container section-container'>
         <H3 text='Alianzas Kodemia' />
         <H2 cyanText='Ellos confían' whiteText='en nosotros' isFirstCyan />
-        <div className='allies-container'>
-          <div className='slider'>
+        <div className="allies-slider">
+          <div className="slider-container">
             {
-              allies.map((ally, index) => {
-                return (
-                  <img
-                    src={ally.image}
-                    key={`ally-${index}`}
-                    className='ally-container'
-                  />
+              allies.map((ally,index)=>{
+                return(
+                  <div className="slide" key={`ally-${index}`}>
+                    <img src={ally.image}
+                    className='ally'
+                    alt={ally.alt} />
+                  </div>
                 )
               })
             }
