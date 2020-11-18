ecd7692da10e8e2ff04fc6b7fc28edaefa042b9a
Arianaomi <arianaomi.lp@gmail.com>
2020-11-17 23:27:58 -0600

    :art: Full alliances-kodemia component

--- components/Pages/Home/AlliancesKodemia.tsx
+++ components/Pages/Home/AlliancesKodemia.tsx
@@ -13,21 +13,23 @@ export interface Props {
   allies: Array<Allies>
 }
 
-export default function AllianceKodemia({ allies }: Props) {
+export default function AllianceKodemia ({ allies }: Props) {
   return (
     <section className='alliances-kodemia'>
       <div className='alliances-kodemia-container section-container'>
         <H3 text='Alianzas Kodemia' />
         <H2 cyanText='Ellos confían' whiteText='en nosotros' isFirstCyan />
-        <div className="allies-slider">
-          <div className="slider-container">
+        <div className='allies-slider'>
+          <div className='slider-container'>
             {
-              allies.map((ally,index)=>{
-                return(
-                  <div className="slide" key={`ally-${index}`}>
-                    <img src={ally.image}
-                    className='ally'
-                    alt={ally.alt} />
+              allies.map((ally, index) => {
+                return (
+                  <div className='slide' key={`ally-${index}`}>
+                    <img
+                      src={ally.image}
+                      className='ally'
+                      alt={ally.alt}
+                    />
                   </div>
                 )
               })
