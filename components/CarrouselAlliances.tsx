7172889924836838faed64f1ff0cd94fa7e9cf6f
Arianaomi <arianaomi.lp@gmail.com>
2020-11-25 23:05:06 -0600

    ADD carrousel

---
+++ components/CarrouselAlliances.tsx
@@ -0,0 +1,62 @@
+
+import React from 'react'
+import Slider from 'react-slick'
+
+export interface Ally {
+  name: string;
+  image: string;
+  alt: string;
+  href: string;
+}
+export interface Props {
+  allies: Array<Ally>;
+}
+
+function SampleArrow () {
+  return <div style={{ display: 'none' }} />
+}
+
+export default function CarrouselAlliances ({ allies = [] }: Props) {
+  const settings = {
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 6000,
+    autoplaySpeed: 10,
+    cssEase: 'linear',
+    nextArrow: <SampleArrow />,
+    prevArrow: <SampleArrow />
+  }
+
+  return (
+    <div className='carrousel-alliances'>
+      <Slider {...settings}>
+        {allies.map((ally, index) => {
+          return ally.href === '' ? (
+            <img
+              key={`ally-${index}`}
+              src={ally.image}
+              className='ally'
+              alt={ally.alt}
+            />
+          ) : (
+            <a
+              key={`ally-${index}`}
+              href={ally.href}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='a-ally'
+            >
+              <img
+                src={ally.image}
+                className='ally'
+                alt={ally.alt}
+              />
+            </a>
+          )
+        })}
+      </Slider>
+    </div>
+  )
+}
--- components/Pages/Home/AlliancesKodemia.tsx
+++ components/Pages/Home/AlliancesKodemia.tsx
@@ -1,17 +1,17 @@
-
 import React from 'react'
+
 // My components
 import H2 from '../../H2'
 import H3 from '../../H3'
-
+import CarrouselAlliances from '../../CarrouselAlliances'
 export interface Ally {
-  name: string
-  image: string
-  alt: string
-  href: string
+  name: string;
+  image: string;
+  alt: string;
+  href: string;
 }
 export interface Props {
-  allies: Array<Ally>
+  allies: Array<Ally>;
 }
 
 export default function AllianceKodemia ({ allies = [] }: Props) {
@@ -20,31 +20,39 @@ export default function AllianceKodemia ({ allies = [] }: Props) {
       <div className='alliances-kodemia-container section-container'>
         <H3 text='Alianzas Kodemia' />
         <H2 cyanText='Ellos confían' whiteText='en nosotros' isFirstCyan />
-        <div className='allies-slider'>
-          <div className='slider-container'>
-            {
-              allies.map((ally, index) => {
-                return (
-                  <div
-                    className='slide'
-                    key={`ally-${index}`}
-                  >
-                    <a
-                      href={ally.href}
-                      target='_blank'
-                      rel='noopener noreferrer'
-                    >
-                      <img
-                        src={ally.image}
-                        className='ally'
-                        alt={ally.alt}
-                      />
-                    </a>
-                  </div>
-                )
-              })
-            }
-          </div>
+        <CarrouselAlliances allies={allies} />
+        <div className='slider-container'>
+          {allies.map((ally, index) => {
+            return ally.href === '' ? (
+              <div
+                className='slide'
+                key={`ally-${index}`}
+              >
+                <img
+                  src={ally.image}
+                  className='ally'
+                  alt={ally.alt}
+                />
+              </div>
+            ) : (
+              <div
+                className='slide'
+                key={`ally-${index}`}
+              >
+                <a
+                  href={ally.href}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <img
+                    src={ally.image}
+                    className='ally'
+                    alt={ally.alt}
+                  />
+                </a>
+              </div>
+            )
+          })}
         </div>
       </div>
     </section>
