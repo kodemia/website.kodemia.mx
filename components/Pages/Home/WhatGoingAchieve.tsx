3a5f8978365041114d4e238b70805a3b24dc5e4e
rrortegaa <rrortegaa@gmail.com>
2020-12-22 18:19:24 -0600

    add react-slick to what-going-achieve component

--- components/Pages/Home/WhatGoingAchieve.tsx
+++ components/Pages/Home/WhatGoingAchieve.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import Slider from 'react-slick'
 
 import Button from '../../Button'
 import H2 from '../../H2'
 import H3 from '../../H3'
+// import { settings } from 'cluster'
 
 export interface Image {
-  src: string
+  image: string
 }
 
 export interface Props {
@@ -13,6 +15,36 @@ export interface Props {
 }
 
 export default function WhatGoingAchieve ({ winnerImages }: Props) {
+  const slider = useRef(null)
+  useEffect(() => {
+    const dots = document.getElementsByClassName('custom-dot')
+    if (dots && dots[0]) {
+      dots[0].style = ''
+    }
+  }, [])
+
+  const settings = {
+    arrows: false,
+    centerMode: true,
+    dots: true,
+    dotsClass: 'custom-dot',
+    fade: true,
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    speed: 500,
+    customPaging: function (i) {
+      return (
+        <div className='div-slider'>
+          <div className='bar'>&nbsp;</div>
+          {i === 0 && <p>wireframe</p>}
+          {i === 1 && <p>code</p>}
+          {i === 2 && <p>mockup</p>}
+        </div>
+      )
+    }
+  }
+
   return (
     <section className='what-going-achieve'>
       <div className='achieves-container section-container'>
@@ -27,19 +59,27 @@ export default function WhatGoingAchieve ({ winnerImages }: Props) {
           </p>
           <Button isPrimary label='Conoce la experiencia completa' />
         </div>
-        <div className='images'>
-          {
-            winnerImages.map((image, index) => {
-              return (
-                <div
-                  className='image'
-                  key={`image-${index}`}
-                >
-                  <img src={image} alt='winner images' />
-                </div>
-              )
-            })
-          }
+        <div className='images-bar-container'>
+          <div className='images'>
+            <Slider {...settings} ref={slider}>
+              {
+                winnerImages.map((image, index) => {
+                  console.log('image', image)
+                  return (
+                    <div
+                      className='image'
+                      key={`image-${index}`}
+                    >
+                      <img src={image.image} alt='winner images' />
+                    </div>
+                  )
+                })
+              }
+            </Slider>
+          </div>
+          {/* <div className='bar'>
+            <input type='checkbox'></input>
+          </div> */}
         </div>
       </div>
     </section>
