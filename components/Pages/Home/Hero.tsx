d53c8bc38349ddf81f007d443d11810ad52700a0
Arianaomi <arianaomi.lp@gmail.com>
2021-01-04 12:31:15 -0600

    :twisted_rightwards_arrows: merge branch  master into login

--- components/Pages/Home/Hero.tsx
+++ components/Pages/Home/Hero.tsx
@@ -7,22 +7,7 @@ import BgVideo from '../../BgVideo'
 export default function Hero () {
   return (
     <div className='hero'>
-<<<<<<< HEAD
       <BgVideo />
-=======
-      <div className='bg-video'>
-        <video
-          className='video'
-          autoPlay
-          loop
-          muted
-        >
-          <source
-            src='https://prismic-io.s3.amazonaws.com/kodemia/d58417e7-a700-4c67-a3e2-5a746d3df0b2_home-background.mp4'
-          />
-        </video>
-      </div>
->>>>>>> master
       <div className='cover'>
         <div className='container'>
           <HeroCard
