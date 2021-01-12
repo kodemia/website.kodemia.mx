3ac3e794551ce26ff69c80261613416161115c23
Arianaomi <arianaomi.lp@gmail.com>
2021-01-11 20:00:17 -0600

    rename variable

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@ const withPlugins = require('next-compose-plugins')
 const withImages = require('next-images')
 
 module.exports = withPlugins([withImages({})])
+
+module.exports = {
+  env: {
+    URL_API: 'https://api-kodemia-mx.vercel.app'
+  }
+}
