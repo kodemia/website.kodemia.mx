86c216349bb957b6b5c2d4ea98f8a279ba7f746c
Arianaomi <arianaomi.lp@gmail.com>
2021-01-14 16:06:36 -0600

    hero js style

---
+++ components/Pages/Bootcamps/Hero.tsx
@@ -0,0 +1,43 @@
+
+import React from 'react'
+// My components
+
+import HeroCard from './HeroCard'
+import BgVideo from '../../BgVideo'
+
+export interface Information {
+  start: string
+  modality: string
+  schedule: string
+}
+
+export interface Props{
+  bootcamp:{
+    title: string
+    subtitle: string
+    description: string
+    information: Array<Information>
+  }
+}
+
+export default function JavascriptLive ({ bootcamp }:Props) {
+  return (
+    <div className='hero-bootcamps'>
+      <div className='columns hero-container'>
+        <div className='column is-full'>
+          <BgVideo />
+          <div className='columns is-justify-content-center cover'>
+            <div className='column  container-card'>
+              <HeroCard bootcamp={bootcamp} />
+              <img
+                src='/icons/icon-scroll.svg'
+                className='arrow-icon'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
--- pages/javascript-live/index.tsx
+++ pages/javascript-live/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+// My components
+import Navbar from '../../components/Navbar'
+import Hero from '../../components/Pages/Bootcamps/Hero'
+// Data
+import HeroData from '../../config/bootcapms/hero-data.json'
 
 export default function JavascriptLive () {
   return (
-    <h1>JAVASCRIPT</h1>
+    <>
+      <Navbar />
+      <Hero bootcamp={HeroData.javascript} />
+    </>
   )
 }
