7bb4fb92f1b2fa4a3534f1d36eea89b7aca36da7
rrortegaa <rrortegaa@gmail.com>
2021-01-18 17:47:03 -0600

    Metrics section added

--- components/Pages/Bootcamps/Metrics.tsx
+++ components/Pages/Bootcamps/Metrics.tsx
@@ -2,26 +2,26 @@ import React from 'react'
 
 import MetricCard from './MetricCard'
 
-export interface Metric {
+export interface Props {
   description: string
   value: string
 }
 
-export interface Props {
-  bootcamp: {
-    metric: Array<Metric>
-  }
-}
+// export interface Props {
+//   bootcamp: {
+//     metric: Array<Metric>
+//   }
+// }
 
-export default function Metrics ({ bootcamp }: Props) {
+export default function Metrics ({ description, value }: Props) {
   return (
     <div className='metrics'>
       <div className='metrics-container'>
-        {bootcamp.metric.map((metric, index) => {
+        {metrics.map((metric, index) => {
           return (
             <MetricCard
-              value={metric.value}
-              description={metric.description}
+              value={value}
+              description={description}
               key={`metric-${index}`}
             />
           )
