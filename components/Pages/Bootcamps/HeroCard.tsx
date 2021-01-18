5a3e4db9b2ba95a637a1aeee7a88b80e22869c7f
Arianaomi <arianaomi.lp@gmail.com>
2021-01-18 15:36:40 -0600

    Add hero of python

--- components/Pages/Bootcamps/HeroCard.tsx
+++ components/Pages/Bootcamps/HeroCard.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 // My components
-import H1 from '../../H1'
+import H2 from '../../H2'
 import H3 from '../../H3'
 
 export interface Information {
@@ -23,8 +23,8 @@ export default function HeroCard ({ bootcamp }: Props) {
     <div className='hero-card-bootcamps'>
       <div className='columns is-flex-direction-column container-data'>
         <div className='column  is-one-third-mobile is-half-tablet is-one-third-desktop container-tittle'>
-          <H3 text='Javascript' />
-          <H1 text='Bootcamp Full Stack JavaScript' />
+          <H3 text={bootcamp.subtitle} />
+          <H2 whiteText={bootcamp.title} cyanText='' isFirstCyan />
         </div>
         <div className='column is-one-third-desktop is-three-fifths-tablet container-description'>
           <p className='description'>
--- pages/python-live/index.tsx
+++ pages/python-live/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+// My components
+import Navbar from '../../components/Navbar'
+import Hero from '../../components/Pages/Bootcamps/Hero'
+import Testimonials from '../../components/Pages/Home/Testimonials'
+import TransformYourLife from '../../components/Pages/Home/TransformYourLife'
+// Data
+import HeroData from '../../config/bootcapms/hero-data.json'
+import TestimonialData from '../../config/testimonials-data.json'
 
 export default function PythonLive () {
   return (
-    <h1>PYTHON</h1>
+    <>
+      <Navbar />
+      <Hero bootcamp={HeroData.python} />
+      <Testimonials testimonials={TestimonialData} />
+      <TransformYourLife />
+    </>
   )
 }
