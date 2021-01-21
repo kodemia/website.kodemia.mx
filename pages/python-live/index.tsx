6af6403d1eb3e7bc135ce9fc64c2e4b4ec381fca
Arianaomi <arianaomi.lp@gmail.com>
2021-01-21 01:40:51 -0600

    add data of python

--- pages/python-live/index.tsx
+++ pages/python-live/index.tsx
@@ -6,17 +6,25 @@ import Metrics from '../../components/Pages/Bootcamps/Metrics'
 import Navbar from '../../components/Navbar'
 import Testimonials from '../../components/Pages/Home/Testimonials'
 import TransformYourLife from '../../components/Pages/Home/TransformYourLife'
+import Program from '../../components/Pages/Bootcamps/Program'
 // Data
 import HeroData from '../../config/bootcamps/hero-data.json'
 import MetricsBootcamps from '../../config/bootcamps/metrics.json'
 import TestimonialData from '../../config/testimonials-data.json'
-
+import ProgramData from '../../config/bootcamps/program-data.json'
 export default function PythonLive () {
   return (
     <>
       <Navbar />
       <Hero bootcamp={HeroData.python} />
       <Metrics metrics={MetricsBootcamps.python} />
+      <Program
+        subtitle={ProgramData.subtitle}
+        cyanText={ProgramData.cyanText}
+        whiteText={ProgramData.whiteText}
+        bootcamp={ProgramData.python}
+
+      />
       <Testimonials testimonials={TestimonialData} />
       <TransformYourLife />
       <Footer />
