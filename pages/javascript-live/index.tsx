96023534654ee44f0995022bf6c646d74bc4c7b4
rrortegaa <rrortegaa@gmail.com>
2021-01-22 01:07:01 -0600

    goals section added in JS and Python bootcamps

--- pages/javascript-live/index.tsx
+++ pages/javascript-live/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 // My components
 import Footer from '../../components/Footer'
+import Goals from '../../components/Pages/Bootcamps/Goals'
 import Hero from '../../components/Pages/Bootcamps/Hero'
 import Metrics from '../../components/Pages/Bootcamps/Metrics'
 import Navbar from '../../components/Navbar'
 import Testimonials from '../../components/Pages/Home/Testimonials'
 import TransformYourLife from '../../components/Pages/Home/TransformYourLife'
 // Data
-import HeroData from '../../config/bootcapms/hero-data.json'
+import GoalsData from '../../config/bootcamps/goals.json'
+import HeroData from '../../config/bootcamps/hero-data.json'
 import MetricsBootcamps from '../../config/bootcamps/metrics.json'
 import TestimonialData from '../../config/testimonials-data.json'
 
@@ -17,6 +19,7 @@ export default function JavascriptLive () {
       <Navbar />
       <Hero bootcamp={HeroData.javascript} />
       <Metrics metrics={MetricsBootcamps.javascript} />
+      <Goals infoCards={GoalsData.cards} />
       <Testimonials testimonials={TestimonialData} />
       <TransformYourLife />
       <Footer />
