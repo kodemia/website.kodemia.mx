ce1dd8c20440843537e863390f5b5f622d1d2d3c
charles <charlesipn@gmail.com>
2021-01-25 14:02:56 -0600

    ADD SEO to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import 'node_modules/slick-carousel/slick/slick-theme.css'
 import 'react-phone-input-2/lib/style.css'
 import 'react-toastify/dist/ReactToastify.css'
 import 'styles/index.scss'
+import Seo from 'components/SEO'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +15,7 @@ function MyApp ({ Component, pageProps }: AppProps) {
       <Head>
         <script async defer src='https://static.cdn.prismic.io/prismic.js?new=true&repo=kodemia' />
       </Head>
+      <Seo />
       <div className='page-container'>
         <Component {...pageProps} />
       </div>
