bb566abcaab1bb2ca62753d3ea4cc6738567f878
arianaomi <arianaomi.lp@gmail.com>
2021-01-29 21:18:50 -0600

    ADD google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import TagManager from 'react-gtm-module'
 
 import 'node_modules/slick-carousel/slick/slick.css'
 import 'node_modules/slick-carousel/slick/slick-theme.css'
@@ -8,7 +10,16 @@ import 'react-phone-input-2/lib/style.css'
 import 'react-toastify/dist/ReactToastify.css'
 import 'styles/index.scss'
 
-function MyApp ({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+
+  const TagManagerArgs = {
+    gtmId: 'GTM-M752N7Z',
+  }
+
+  useEffect(() => {
+    TagManager.initialize(TagManagerArgs)
+  }, [])
+
   return (
     <>
       <Head>
