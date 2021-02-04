55e0ce73b15ace09978970ca8388a0b0716cfdc2
arianaomi <arianaomi.lp@gmail.com>
2021-02-04 00:51:25 -0600

    clean the code

--- components/CarrouselAlliances.tsx
+++ components/CarrouselAlliances.tsx
@@ -14,11 +14,11 @@ export interface Props {
   hasContent?: boolean
 }
 
-function SampleArrow() {
+function SampleArrow () {
   return <div style={{ display: 'none' }} />
 }
 
-export default function CarrouselAlliances({ allies = [], hasContent }: Props) {
+export default function CarrouselAlliances ({ allies = [], hasContent }: Props) {
   const settings = {
     infinite: true,
     slidesToShow: 4,
