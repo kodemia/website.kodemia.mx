8c82a34765d291173a1df1edde7c195040c094a7
arianaomi <arianaomi.lp@gmail.com>
2021-02-04 13:57:35 -0600

    full next developer section

--- components/Pages/Companies/NextDeveloper.tsx
+++ components/Pages/Companies/NextDeveloper.tsx
@@ -17,7 +17,7 @@ export default function NextDeveloper () {
           <Button
             label='Contáctanos'
             hasWhiteBg
-            link='/'
+            link='#'
           />
         </div>
       </div>
