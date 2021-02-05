89e5a039588e9fd2bcb6ff9add89ee7cef1f1acb
arianaomi <arianaomi.lp@gmail.com>
2021-02-05 00:51:25 -0600

    change path

--- pages/empresas.tsx
+++ pages/empresas.tsx
@@ -1,15 +1,15 @@
 
 import React from 'react'
 // My components
-import Seo from '../components/SEO'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Pages/Companies/Hero'
-import AlliancesKodemia from '../components/Pages/Home/AlliancesKodemia'
-import Footer from '../components/Footer'
+import Seo from 'components/SEO'
+import Navbar from 'components/Navbar'
+import Hero from 'components/Pages/Companies/Hero'
+import AlliancesKodemia from 'components/Pages/Home/AlliancesKodemia'
+import Footer from 'components/Footer'
 // Data
-import AlliesData from '../config/allies-data.json'
+import AlliesData from 'config/allies-data.json'
 
-export default function Empresas () {
+export default function Empresas() {
   return (
     <>
       <Seo />
