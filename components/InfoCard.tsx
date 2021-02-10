38d4470b55b8cec72049da154c20838395507177
rrortegaa <rrortegaa@gmail.com>
2021-02-09 21:42:23 -0600

    Added FindKoder component and changed InfoCard component

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -5,15 +5,16 @@ export interface Props {
   icon: string,
   title: string,
   text: string
-
+  hasChildren?: boolean
 }
 
-export default function InfoCard ({ icon, title, text }: Props) {
+export default function InfoCard ({ hasChildren, icon, title, text }: Props) {
   return (
     <div className='info-card'>
       <img src={icon} className='ikon' />
       <h2 className='info-card-title'>{title}</h2>
       <p className='text'>{text}</p>
     </div>
+    { hasChildren? <>: '' }
   )
 }
--- components/Pages/Companies/FindKoder.tsx
+++ components/Pages/Companies/FindKoder.tsx
@@ -2,15 +2,37 @@ import React from 'react'
 
 import H2 from 'components/H2'
 import H3 from 'components/H3'
-// import InfoCard from 'components/InfoCard'
+import InfoCard, { Props as InfoCardProps } from 'components/InfoCard'
 // import BootcampCard from 'components/BootcampCard'
 
-export default function FindKoder () {
+export interface Props {
+  detail: string
+  image: string
+  infoCards: Array<InfoCardProps>
+}
+
+export default function FindKoder ({ detail, image, infoCards }: Props) {
   return (
     <section className='find-koder'>
       <div className='find-koder-container section-container'>
         <H3 text='¿Cómo encuentro al Koder perfecto para mi empresa?' />
         <H2 whiteText='Encuentra al' cyanText='programador ideal' />
+        <img src={image} alt='koder ideal' />
+        <div className='cards-container'>
+          {
+            infoCards.map((infoCard, index) => {
+              return (
+                <InfoCard
+                  key={`card-${index}`}
+                  icon={infoCard.icon}
+                  title={infoCard.title}
+                  text={infoCard.text}
+                />
+                
+              )
+            })
+          }
+        </div>
       </div>
     </section>
   )
