f5c441dadef07b2715d2b9f50a37ac484955ec64
rrortegaa <rrortegaa@gmail.com>
2021-02-10 00:26:11 -0600

    added FindAllTalent component

---
+++ components/Pages/Companies/FindAllTalent.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import H2 from 'components/H2'
+import H3 from 'components/H3'
+import InfoCard, { Props as InfoCardProps } from 'components/InfoCard'
+
+export interface Props {
+  detail: string
+  infoCards: Array<InfoCardProps>
+}
+
+export default function FindAllTalent ({ detail, infoCards }: Props) {
+  return (
+    <section className='find-all-talent'>
+      <div className='find-talent-container section-container'>
+        <div className='head'>
+          <H3 text='¿Con qué empresas trabajamos' />
+          <H2 whiteText='Ayudamos a encontrar talento ' cyanText='a todo nivel' />
+          <p>
+            {detail}
+          </p>
+        </div>
+        <div className='cards'>
+          {
+            infoCards.map((infoCard, index) => {
+              return (
+                <InfoCard
+                  key={`card-${index}`}
+                  icon={infoCard.icon}
+                  title={infoCard.title}
+                  text={infoCard.text}
+                />
+              )
+            })
+          }
+        </div>
+      </div>
+    </section>
+  )
+}
