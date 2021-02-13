31f710cc2c5e1af0c7771ed5f29ef4a4e7a5cc1e
arianaomi <arianaomi.lp@gmail.com>
2021-02-12 18:40:28 -0600

    add cursor: pointer

--- pages/empresas.tsx
+++ pages/empresas.tsx
@@ -1,16 +1,15 @@
 // My components
-import AlliancesKodemia from '../components/Pages/Home/AlliancesKodemia'
+import AlliancesKodemia from 'components/Pages/Home/AlliancesKodemia'
 import Footer from 'components/Footer'
-import Hero from '../components/Pages/Companies/Hero'
+import Hero from 'components/Pages/Companies/Hero'
 import Navbar from 'components/Navbar'
-import Seo from '../components/SEO'
+import Seo from 'components/SEO'
 import Talent from 'components/Pages/Companies/TalentDevelopment'
 import NextDeveloper from 'components/Pages/Companies/NextDeveloper'
 import ChooseAKoder from 'components/Pages/Companies/ChooseAKoder'
 import CollaborateWithKodemia from 'components/Pages/Companies/CollaborateWithKodemia'
-
 // Data
-import AlliesData from '../config/allies-data.json'
+import AlliesData from 'config/allies-data.json'
 import TalentData from 'config/companies/talent-data.json'
 import ChooseKoder from 'config/companies/choose-koder-data.json'
 
