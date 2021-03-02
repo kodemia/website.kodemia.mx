d158ee129302a919062d16175615eff25a463f0b
arianaomi <arianaomi.lp@gmail.com>
2021-03-01 19:47:20 -0600

    change max-width on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar () {
 
   return (
     <nav className='navbar' role='navigation' aria-label='main navigation'>
-      <div className='container'>
+      <div className='navbar-container is-flex'>
         <div className='navbar-brand'>
           <Link href='/'>
             <a className='navbar-item'>
