1e99a8102315e0c0ae9bdfc0c94fc2f3f97adec6
arianaomi <arianaomi.lp@gmail.com>
2021-03-12 01:18:58 -0600

    change font-size of home

--- components/Pages/Home/KodemiaExperience.tsx
+++ components/Pages/Home/KodemiaExperience.tsx
@@ -21,7 +21,7 @@ export interface Props {
   videos: Array<Video>
 }
 
-export default function KodemiaExperience ({ videos = [] }:Props) {
+export default function KodemiaExperience ({ videos = [] }: Props) {
   const [activeVideoIndex, setActiveVideoIndex] = useState<number>()
   const [isActive, setIsActive] = useState(false)
   const gridRef = useRef(null)
@@ -39,11 +39,13 @@ export default function KodemiaExperience ({ videos = [] }:Props) {
     <section className='kodemia-experience'>
       <div className='kodemia-experience-container section-container'>
         <H3 text='La experiencia Kodemia' />
-        <H2
-          cyanText='Conoce la historia'
-          whiteText='de nuestros graduados'
-          isFirstCyan
-        />
+        <div className='title'>
+          <H2
+            cyanText='Conoce la historia'
+            whiteText='de nuestros graduados'
+            isFirstCyan
+          />
+        </div>
         <p className='description'>Es más importante contar historias que números. Buscamos que los alumnos que salen de Kodemia transformen su vida.</p>
         <div
           ref={gridRef} className={classnames('videos-container', {
