869a6339889c131f99276f553091eaf4dfc2b526
charles <charles@kodemia.mx>
2021-07-15 02:09:35 -0500

    ADD TODO

--- pages/brochures/[program]/index.tsx
+++ pages/brochures/[program]/index.tsx
@@ -12,6 +12,7 @@ export function getServerSideProps (context: GetServerSidePropsContext) {
   const program: Program = context?.params?.program as Program ?? 'javascript-live'
   const version: BrochureFormat = context?.query?.version as BrochureFormat ?? 'mobile'
 
+  // TODO: remove hardcoded version when 2021v2 updates are applied to the website
   context.res
     .writeHead(302, { Location: brochures['2021v1'][program][version] })
     .end()
