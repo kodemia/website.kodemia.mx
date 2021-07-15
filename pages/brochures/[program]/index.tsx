77b3e75440499bec3f7d87002e77fac4486e5eca
charles <charles@kodemia.mx>
2021-07-15 02:02:26 -0500

    clean old version

--- pages/brochures/[program]/index.tsx
+++ pages/brochures/[program]/index.tsx
@@ -1,19 +1,16 @@
 
 import { GetServerSidePropsContext } from 'next'
+import { Program, BrochureFormat } from 'types/common'
 
-// TODO: fetch this data from prismic
 import brochures from 'config/brochure-links.json'
-
-type Bootcamp = 'javascript-live'
-type Version = 'desktop' | 'mobile'
 
 export default function DownloadBrochure () {
   return null
 }
 
 export function getServerSideProps (context: GetServerSidePropsContext) {
-  const program: Bootcamp = context?.params?.program as Bootcamp ?? 'javascript-live'
-  const version: Version = context?.query?.version as Version ?? 'mobile'
+  const program: Program = context?.params?.program as Program ?? 'javascript-live'
+  const version: BrochureFormat = context?.query?.version as BrochureFormat ?? 'mobile'
 
   context.res
     .writeHead(302, { Location: brochures['2021v1'][program][version] })
