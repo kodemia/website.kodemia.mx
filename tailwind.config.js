4b8bcd4c37c67f682aec4adb7ca94874e8625695
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2021-10-27 05:02:29 -0500

    :construction: removing Bulma and style files

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,6 +70,9 @@ module.exports = {
       lineHeight: {
         h1: '1.07'
       },
+      maxWidth: {
+        'sm-kd': '220px'
+      },
       minHeight: {
         mini: '16px',
         'sm-kd': '465px'
@@ -77,8 +80,11 @@ module.exports = {
       minWidth: {
         'sm-kd': '220px'
       },
-      maxWidth: {
-        'sm-kd': '220px'
+      screens: {
+        mob: '320px', // Work around to deal with bulma spacing utilities TODO: remove when removing bulma
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1280px'
       },
       webkitScrollbar: {
         width: '6px'
