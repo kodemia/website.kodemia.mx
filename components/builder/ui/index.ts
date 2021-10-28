38716297e6721f9b503f0e44fafd5fff717a247a
charlesKodemia <charles@kodemia.mx>
2021-10-28 12:22:50 -0500

    remove simple bullet list

--- components/builder/ui/index.ts
+++ components/builder/ui/index.ts
@@ -1,9 +1,8 @@
 
 import Button from 'components/builder/ui/Button.builder'
 import Navbar from 'components/builder/ui/NavBar.builder'
-import SimpleBulletList from 'components/builder/ui/SimpleBulletList.builder'
 
 export default {
   name: 'UI',
-  items: [ Button, Navbar, SimpleBulletList ]
+  items: [ Button, Navbar ]
 }
