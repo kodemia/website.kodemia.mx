5ff46c38fae6adfb831f6c4a68ed3f67792908ef
arianaomi <arianaomi.lp@gmail.com>
2021-11-04 21:21:34 -0600

    variables

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   env: {
     API_URL: process.env.API_URL,
-    FB_PIXEL_ID: 'XXX',
-    GTM_CONTAINER_ID: 'XXX',
-    MIXPANEL_TOKEN: 'XXX',
-    GA_TRACKING_ID: 'XXX',
-    HOTJAR_ID: 'XXX'
+    BUILDER_API_KEY: process.env.BUILDER_API_KEY,
+    FB_PIXEL_ID: process.env.FB_PIXEL_ID,
+    GTM_CONTAINER_ID: process.env.GTM_CONTAINER_ID,
+    MIXPANEL_TOKEN: process.env.MIXPANEL_TOKEN,
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
+    HOTJAR_ID: process.env.HOTJAR_ID
   },
   images: {
     domains: [
