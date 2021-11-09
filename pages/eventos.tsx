77a42574034f7d179aaa241ec7d1136a4668a53f
arianaomi <arianaomi.lp@gmail.com>
2021-11-09 17:05:25 -0600

    heigth

--- pages/eventos.tsx
+++ pages/eventos.tsx
@@ -12,78 +12,78 @@ import Card from 'components/Card'
 
 dayjs.locale(es);
 export interface Event {
-  name: string
-  date: Date
-  isLive: Boolean
-  isPrivate: Boolean
-  link: string
+	name: string
+	date: Date
+	isLive: Boolean
+	isPrivate: Boolean
+	link: string
 }
 
 export interface Props {
-  events: Array<Event>
+	events: Array<Event>
 }
 
 export const getStaticProps = async () => {
-  const date = Date.now()
-  const results = await builder.getAll('evento')
-  const events = results.map(event => event.data)
-    .sort((a: any, b: any) => a.date - b.date)
-    .filter((event: any) => event.date > date)
+	const date = Date.now()
+	const results = await builder.getAll('evento')
+	const events = results.map(event => event.data)
+		.sort((a: any, b: any) => a.date - b.date)
+		.filter((event: any) => event.date > date)
 
-  return {
-    props: {
-      events
-    },
-    revalidate: 300
-  }
+	return {
+		props: {
+			events
+		},
+		revalidate: 300
+	}
 }
 
 export default function Eventos({ events }: Props) {
-  return (
-    <>
-      <Navbar />
-      <div className={classNames(
-        'bg-no-repeat bg-cover bg-events h-80',
-        'flex flex-col justify-center xl:items-center'
-      )}>
-        <div className={classNames('px-14 max-w-1086-px w-full xl:px-0')}>
-          <H5>
-            Eventos Kodemia
-          </H5>
-          <div>
-            <H3
-              whiteText='No te pierdas'
-              cyanText='ninguno'
-            />
-          </div>
-        </div>
-      </div>
-      <main className={classNames(
-        'bg-black-kd p-12 lg:p-14',
-        'flex justify-center xl:items-center'
-      )}>
-        <section className={classNames(
-          'max-w-1086-px w-full ',
-          'flex flex-wrap justify-between'
-        )}>
-          {
-            events.map((event, index) => (
-              <article key={`event-${index}`}
-                className={classNames('w-full md:w-80 ', 'my-5')}>
-                <Card key={index}
-                  name={event.name}
-                  date={dayjs(event.date).format('DD MMMM ').toString()}
-                  schedule={`${dayjs(event.date).format('HH:mm').toString()} hrs.`}
-                  text={event.isLive ? 'Conexión remota vía streaming' : 'Presencial, te esperamos 😎'}
-                  btnLabel={event.isPrivate ? 'Regístrate' : 'Ver evento'}
-                  link={event.link}
-                />
-              </article>
-            ))
-          }
-        </section>
-      </main>
-      <Footer />
-    </>
-  )
+	return (
+		<>
+			<Navbar />
+			<header className={classNames(
+				'bg-no-repeat bg-cover bg-events h-80 min-h-430-px',
+				'flex flex-col justify-center xl:items-center'
+			)}>
+				<div className={classNames('px-14 max-w-1086-px w-full xl:px-0')}>
+					<H5>
+						Eventos Kodemia
+					</H5>
+					<div>
+						<H3
+							whiteText='No te pierdas'
+							cyanText='ninguno'
+						/>
+					</div>
+				</div>
+			</header>
+			<main className={classNames(
+				'bg-black-kd p-12 lg:p-14',
+				'flex justify-center xl:items-center'
+			)}>
+				<section className={classNames(
+					'max-w-1086-px w-full ',
+					'flex flex-wrap justify-between'
+				)}>
+					{
+						events.map((event, index) => (
+							<article key={`event-${index}`}
+								className={classNames('w-full md:w-80 ', 'my-5')}>
+								<Card key={index}
+									name={event.name}
+									date={dayjs(event.date).format('DD MMMM ').toString()}
+									schedule={`${dayjs(event.date).format('HH:mm').toString()} hrs.`}
+									text={event.isLive ? 'Conexión remota vía streaming' : 'Presencial, te esperamos 😎'}
+									btnLabel={event.isPrivate ? 'Regístrate' : 'Ver evento'}
+									link={event.link}
+								/>
+							</article>
+						))
+					}
+				</section>
+			</main>
+			<Footer />
+		</>
+	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,99 +1,100 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      screens: {
-        mob: '320px', // Work around to deal with bulma spacing utilities TODO: remove when removing bulma
-        tablet: '640px',
-        laptop: '1024px',
-        desktop: '1280px'
-      },
-      animation: {
-        cursor: 'cursor alternate linear 0.8s infinite'
-      },
-      borderRadius: {
-        card: '10px',
-        'gray-card': '30px'
-      },
-      borderWidth: {
-        'text-cursor': '3.5px'
-      },
-      colors: {
-        'black-kd': {
-          dark: '#0E0E0E',
-          DEFAULT: '#181818',
-          light: '#343434',
-          trasparent: 'rgba(24, 24, 24, 0.85)'
-        },
-        'cyan-kd': {
-          dark: '#07BABA',
-          DEFAULT: '#00FFFF'
-        },
-        'gray-kd': {
-          dark: '#2f2f2f',
-          DEFAULT: '#4E4E4E',
-          light: '#828282',
-          lighter: '#bdbdbd'
-        },
-        'green-kd': {
-          dark: '#AAD64C',
-          DEFAULT: '#95e3a5'
-        },
-        'red-kd': {
-          DEFAULT: '#E95249',
-          light: '#fa867f'
-        },
-        'violet-kd': {
-          DEFAULT: '#BB2BFF'
-        },
-        'white-kd': {
-          DEFAULT: '#FBFBFB'
-        }
-      },
-      fontFamily: {
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans]
-      },
-      fontSize: {
-        h1: '40px',
-        h2: '32px',
-        h3: '28px',
-        h4: '24px',
-        h5: '20px',
-        mini: '10px'
-      },
-      keyframes: {
-        cursor: {
-          '0%, 100%': {
-            borderBottomColor: 'transparent'
-          },
-          '50%': {
-            borderBottomColor: 'unset'
-          }
-        }
-      },
-      lineHeight: {
-        h1: '1.07'
-      },
-      minHeight: {
-        mini: '16px',
-        'sm-kd': '465px'
-      },
-      minWidth: {
-        'sm-kd': '220px'
-      },
-      maxWidth: {
-        'sm-kd': '220px',
-        '1086-px': '1086px'
-      },
-      webkitScrollbar: {
-        width: '6px'
-      },
-      backgroundImage: {
-        'events': "url('https://cdn.kodemia.mx/images/eventos/eventos-desktop.jpg')"
-      }
-    }
-  }
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			screens: {
+				mob: '320px', // Work around to deal with bulma spacing utilities TODO: remove when removing bulma
+				tablet: '640px',
+				laptop: '1024px',
+				desktop: '1280px'
+			},
+			animation: {
+				cursor: 'cursor alternate linear 0.8s infinite'
+			},
+			borderRadius: {
+				card: '10px',
+				'gray-card': '30px'
+			},
+			borderWidth: {
+				'text-cursor': '3.5px'
+			},
+			colors: {
+				'black-kd': {
+					dark: '#0E0E0E',
+					DEFAULT: '#181818',
+					light: '#343434',
+					trasparent: 'rgba(24, 24, 24, 0.85)'
+				},
+				'cyan-kd': {
+					dark: '#07BABA',
+					DEFAULT: '#00FFFF'
+				},
+				'gray-kd': {
+					dark: '#2f2f2f',
+					DEFAULT: '#4E4E4E',
+					light: '#828282',
+					lighter: '#bdbdbd'
+				},
+				'green-kd': {
+					dark: '#AAD64C',
+					DEFAULT: '#95e3a5'
+				},
+				'red-kd': {
+					DEFAULT: '#E95249',
+					light: '#fa867f'
+				},
+				'violet-kd': {
+					DEFAULT: '#BB2BFF'
+				},
+				'white-kd': {
+					DEFAULT: '#FBFBFB'
+				}
+			},
+			fontFamily: {
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+			},
+			fontSize: {
+				h1: '40px',
+				h2: '32px',
+				h3: '28px',
+				h4: '24px',
+				h5: '20px',
+				mini: '10px'
+			},
+			keyframes: {
+				cursor: {
+					'0%, 100%': {
+						borderBottomColor: 'transparent'
+					},
+					'50%': {
+						borderBottomColor: 'unset'
+					}
+				}
+			},
+			lineHeight: {
+				h1: '1.07'
+			},
+			minHeight: {
+				mini: '16px',
+				'sm-kd': '465px',
+				'430-px': '430px'
+			},
+			minWidth: {
+				'sm-kd': '220px'
+			},
+			maxWidth: {
+				'sm-kd': '220px',
+				'1086-px': '1086px'
+			},
+			webkitScrollbar: {
+				width: '6px'
+			},
+			backgroundImage: {
+				'events': "url('https://cdn.kodemia.mx/images/eventos/eventos-desktop.jpg')"
+			}
+		}
+	}
 }
