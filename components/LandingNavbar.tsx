364079d574ca754502591acabccf24d346b0d52f
arianaomi <arianaomi.lp@gmail.com>
2021-11-18 07:42:38 -0600

    fix image width

--- components/LandingNavbar.tsx
+++ components/LandingNavbar.tsx
@@ -2,34 +2,36 @@
 import Image from 'next/image'
 
 type Props = {
-  mobileLogo: string
-  desktopLogo: string
+	mobileLogo: string
+	desktopLogo: string
 }
 
-export default function LandingNavbar (props: Props) {
-  return (
-    <nav className='h-16 bg-black-kd-trasparent mb-0 flex mob:px-5 sm:px-12 z-50 relative'>
-      <figure
-        data-component='LandingNavbar-logo'
-        className='container flex'
-      >
-        <div className='sm:hidden w-28 my-auto'>
-          <Image
-            src={props.mobileLogo}
-            layout='responsive'
-            width={112}
-            height={36}
-          />
-        </div>
-        <div className='hidden sm:block w-96 my-auto'>
-          <Image
-            src={props.desktopLogo}
-            layout='responsive'
-            width={384}
-            height={36}
-          />
-        </div>
-      </figure>
-    </nav>
-  )
+export default function LandingNavbar(props: Props) {
+	return (
+		<nav className='h-16 bg-black-kd-trasparent mb-0 flex mob:px-5 sm:px-12 z-50 relative'>
+			<figure
+				data-component='LandingNavbar-logo'
+				className='container flex'
+			>
+				<div className='sm:hidden w-28 my-auto'>
+					<Image
+						src={props.mobileLogo}
+						layout='responsive'
+						width={112}
+						height={36}
+						objectFit='contain'
+					/>
+				</div>
+				<div className='hidden sm:block w-96 my-auto'>
+					<Image
+						src={props.desktopLogo}
+						layout='responsive'
+						width={384}
+						height={36}
+						objectFit='contain'
+					/>
+				</div>
+			</figure>
+		</nav>
+	)
 }
