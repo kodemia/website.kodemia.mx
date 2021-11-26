b61771163ce327086f73d79856c4fa015f1cf162
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2021-11-26 01:19:17 -0600

    :bug: change alignment into card

--- components/CardEvent.tsx
+++ components/CardEvent.tsx
@@ -5,55 +5,65 @@ import H4 from 'components/H4'
 import Button from 'components/Button'
 
 export interface Props {
-	name: string
-	date: string
-	schedule: string
-	text: string
-	btnLabel: string
-	link: string
+  name: string
+  date: string
+  schedule: string
+  text: string
+  btnLabel: string
+  link: string
+  className: string
 }
 
 export default function Card({
-	name,
-	date,
-	schedule,
-	text,
-	btnLabel,
-	link
+  name,
+  date,
+  schedule,
+  text,
+  btnLabel,
+  link,
+  className
 }: Props) {
-	return (
-		<div className={classNames(
-			'border border-solid rounded-xl',
+  return (
+    <div className={classNames(
+      className,
+      'border border-solid rounded-xl',
       'border-gray-kd-light hover:border-cyan-kd',
-      'h-full',
-			'p-4',
-		)}>
-
-      <div className={classNames('md:h-36')}>
-				<H4>
-					{name}
-				</H4>
-			</div>
-			<h5 className={classNames(
-        'text-cyan-kd text-h5 font-medium',
-        'mt-1'
+      'flex flex-col justify-between',
+      'p-4',
+      'w-full'
+    )}>
+      <div>
+        <H4>
+          {name}
+        </H4>
+      </div>
+      <div className={classNames('mt-4')}>
+        <h5 className={classNames(
+          // 'mt-1',
+          'text-cyan-kd text-h5 font-medium'
         )}
         >
-				{date}
-			</h5>
-			<div className={classNames('my-3')}>
-				<p className={classNames('text-gray-kd-light font-medium text-h5')}>
-					{schedule}
-				</p>
-				<p className={classNames('text-gray-kd-light font-medium')}>
-					{text}
-				</p>
-			</div>
-			<Button
-				isPrimary
-				label={btnLabel}
-				href={link}
-			/>
-		</div>
-	)
+          {date}
+        </h5>
+        <p className={classNames(
+          'text-gray-kd-light text-h5 font-medium'
+        )}
+        >
+          {schedule}
+        </p>
+        <p className={classNames(
+          'mb-5',
+          'text-gray-kd-light font-medium'
+        )}
+        >
+          {text}
+        </p>
+        <Button
+          isPrimary
+          label={btnLabel}
+          href={link}
+        />
+      </div>
+    </div>
+  )
 }
