33be529602c8c0f19518e8dd79aee894faee144b
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2022-01-26 01:02:57 -0600

    :hammer: added new Body component for headings

---
+++ components/Body.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import classNames from 'classnames'
+
+export interface Props {
+  children: React.ReactNode
+}
+
+export default function Body ({ children }: Props):JSX.Element {
+  return (
+    <p className={classNames(
+      'text-brand-gray-light',
+      'text-brand-body',
+      'font-medium',
+      'whitespace-pre-wrap',
+    )}>
+
+    </p>
+  )
+}
