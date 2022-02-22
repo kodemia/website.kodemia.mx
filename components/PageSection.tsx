9d92101f226ea3d311f10a97e9d5c544da8d1cb9
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2022-02-22 06:24:52 -0600

    :lipstick: added sectionClassName and contentClassName props into PageSection component

--- components/PageSection.tsx
+++ components/PageSection.tsx
@@ -3,21 +3,23 @@ import classNames from 'classnames'
 
 export interface Props {
   children: React.ReactNode
-  className?: string
+  sectionClassName?: string
+  contentClassName?: string
 }
 
-export default function PageContainer ({children, className}: Props):JSX.Element {
+export default function PageContainer ({children, sectionClassName, contentClassName}: Props):JSX.Element {
   return (
     <section className={classNames(
       'flex justify-center',
       'pt-20 md:pt-24',
       'px-6 md:px-16 lg:px-12',
       'pb-10 md:pb-16 ',
-      className
+      sectionClassName
     )}>
       <div className={classNames(
         'lg:max-w-screen-xl',
-        'w-full'
+        'w-full',
+        contentClassName
       )}>
         {children}
       </div>
