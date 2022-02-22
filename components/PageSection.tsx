44fd08ba0db6bf11203d6d922aaf300d88cdc216
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2022-02-22 06:17:43 -0600

    :pencil2: fix typos into PageSection component and into package.json

--- components/PageSection.tsx
+++ components/PageSection.tsx
@@ -9,11 +9,11 @@ export interface Props {
 export default function PageContainer ({children, className}: Props):JSX.Element {
   return (
     <section className={classNames(
-      className,
       'flex justify-center',
       'pt-20 md:pt-24',
       'px-6 md:px-16 lg:px-12',
-      'pb-10 md:pb-16 '
+      'pb-10 md:pb-16 ',
+      className
     )}>
       <div className={classNames(
         'lg:max-w-screen-xl',
