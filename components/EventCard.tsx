c35e992ab194eaad2ad00a4d9e571c7a06511fa8
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2022-02-24 02:08:42 -0600

    :bug: fix EventCard bug with justify content

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -26,26 +26,32 @@ export default function EventCard ({
   className
 }: Props):JSX.Element {
   return (
-    <Card className={classNames(className)} isBordered>
+    <Card
+      className={classNames(
+        'flex flex-col justify-between',
+        className
+    )}
+      isBordered
+    >
         <H4>
           {name}
         </H4>
       <div className='mt-4'>
         <h5 className={classNames(
-          'text-cyan-kd text-h5 font-medium'
+          'text-brand-primary text-brand-h5 font-medium'
         )}
         >
           {date}
         </h5>
         <p className={classNames(
-          'text-gray-kd-light text-h5 font-medium'
+          'text-brand-gray-light text-brand-h5 font-medium'
         )}
         >
           {schedule}
         </p>
         <p className={classNames(
           'mb-5',
-          'text-gray-kd-light font-medium'
+          'text-brand-gray-light text-base font-medium'
         )}
         >
           {text}
