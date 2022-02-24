82f6c1d713a7f00feac331d3e31ce6a2b43257a3
arianaomi <arianaomi.lp@gmail.com>
2022-02-23 23:34:11 -0600

    removes spaces

--- lib/tracker.ts
+++ lib/tracker.ts
@@ -11,20 +11,20 @@ type UserTraits = {
 
 let analytics: AnalyticsInstance
 
-function init() {
+function init () {
   analytics = Analytics()
 }
 
-function getUserTraits(): UserTraits {
+function getUserTraits (): UserTraits {
   return analytics.user('traits')
 }
 
-export function onPageView() {
+export function onPageView () {
   init()
   analytics.page()
 }
 
-export function onLogin(email: string) {
+export function onLogin (email: string) {
   init()
   const {
     id: userId,
@@ -43,32 +43,32 @@ export function onLogin(email: string) {
     ({ payload }) => analytics.track('User Log In', payload.traits))
 }
 
-export function onLogOut() {
+export function onLogOut () {
   init()
   analytics.track('User Log Out')
   analytics.reset()
 }
 
-export function onTrapPageLoad() {
+export function onTrapPageLoad () {
   init()
   const user = getUserTraits()
   analytics.track('Trap Page Shown', user)
 }
 
-export function onExpiredUserClickTrapPageContinueButton() {
+export function onExpiredUserClickTrapPageContinueButton () {
   init()
   const user = getUserTraits()
   analytics.track('Trap Page Whatsapp Button Click', user)
 }
 
-export function onAuthReset() {
+export function onAuthReset () {
   init()
   const user = getUserTraits()
   analytics.track('Auth reset', user)
   analytics.reset()
 }
 
-export function onFooterButtonClicked(buttonName: 'Whatsapp' | 'Telegram') {
+export function onFooterButtonClicked (buttonName: 'Whatsapp' | 'Telegram') {
   init()
   const user = getUserTraits()
   analytics.track(`Footer ${buttonName} Button Clicked`, user)
@@ -86,7 +86,7 @@ export type FooterLinkName = 'Facebook'
   | 'EMail'
   | 'Privacy Policy'
 
-export function onFooterLinkClicked(linkName: FooterLinkName) {
+export function onFooterLinkClicked (linkName: FooterLinkName) {
   init()
   const user = getUserTraits()
   analytics.track(`Footer ${linkName} Link Clicked`, user)
