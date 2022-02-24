b827205021665b81c2496fc2c8a2de722d7f5a47
arianaomi <arianaomi.lp@gmail.com>
2022-02-24 00:31:12 -0600

    remove bulma

--- components/Pages/Home/TransformYourLife.tsx
+++ components/Pages/Home/TransformYourLife.tsx
@@ -1,25 +1,36 @@
 import React from 'react'
-
+import ClassNames from 'classnames'
+// My components
+import PageSection from 'components/PageSection'
 import Button from 'components/Button'
 import H2 from 'components/H2'
 
 export default function TransformYourLife () {
   return (
-    <section className='transform-your-life'>
-      <div className='transform-your-life-container section-container'>
-        <div className='title-container'>
+    <section>
+      <PageSection
+        sectionClassName='bg-brand-primary-dark'
+        contentClassName='md:flex md:justify-between md:items-center '
+      >
+        <div className={ClassNames(
+          'text-center md:text-left',
+          'mb-10 md:mb-0 md:pb-4 md:w-2/5'
+        )}>
           <H2>
             ¿Listo para transformar tu vida programando?
           </H2>
         </div>
-        <div className='btn-container'>
+        <div className={ClassNames(
+          'mb-10 md:mb-0',
+          'md:w-2/4 lg:max-w-xs'
+        )}>
           <Button
             label='Aplica ahora'
             hasWhiteBg
             link='/aplicar'
           />
         </div>
-      </div>
+      </PageSection>
     </section>
   )
 }
