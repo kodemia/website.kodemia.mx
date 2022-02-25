8d5dc5e659c9f48fe03bf38db7ab3ae7c7c36828
arianaomi <arianaomi.lp@gmail.com>
2022-02-25 01:41:34 -0600

    navbar 90%

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,12 +11,13 @@ import Auth from 'lib/auth'
 import * as tracker from 'lib/tracker'
 import classNames from 'classnames'
 
-export default function Navbar() {
+export default function Navbar () {
   const [isActive, setIsActive] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const router = useRouter()
   useEffect(() => {
     setIsLoggedIn(!!Auth.get())
+    console.log('cargo',isActive)
   })
 
   const signOut = () => {
@@ -27,9 +28,16 @@ export default function Navbar() {
   }
 
   return (
-    <nav className={classNames('navbar bg-yellow-200 fixed z-40 w-full top-0 flex justify-center px-6 md:px-16 lg:px-12' )}role='navigation' aria-label='main navigation'>
-      <div className={classNames('lg:max-w-screen-xl w-full bg-green-200')}>
-        <div className={classNames('')}>
+    <nav className={classNames(
+      'bg-brand-black-transparent',
+      'fixed z-40 w-full top-0',
+      'flex justify-center',
+      'px-6 md:px-16 lg:px-12 py-4'
+    )}>
+      <div className={classNames('lg:max-w-screen-xl w-full',
+        'flex justify-between flex-col lg:flex-row '
+      )}>
+        <div className={classNames(' w-full lg:w-36 flex justify-between')}>
           <Link href='/'>
             <a className='navbar-item'>
               <img
@@ -43,37 +51,48 @@ export default function Navbar() {
           <a
             onClick={() => {
               setIsActive(!isActive)
+              console.log(isActive)
             }}
             role='button'
             className={classNames(
-              'navbar-burger',
-              'burger',
-              {
-                'is-active': isActive
-              }
+              'h-full  lg:hidden flex flex-col',
             )}
-            aria-label='menu'
-            aria-expanded='false'
+            // aria-label='menu'
+            // aria-expanded='false'
           >
-            <span aria-hidden='true' />
-            <span aria-hidden='true' />
-            <span aria-hidden='true' />
+          { !isActive && (
+            <>
+             <div className={classNames('h-2 w-6 border-b-2 border-solid border-brand-complementary')}/>
+             <div className={classNames('h-2 w-6 border-b-2 border-solid border-brand-complementary')}/>
+             <div className={classNames('h-2 w-6 border-b-2 border-solid border-brand-complementary')}/>
+            </>
+          )
+          }
+           { isActive && (
+            <div className='bg-pink-200'>
+             <div className={classNames('h-2 w-6 border-b-2 border-solid border-brand-complementary')}/>
+             <div className={classNames('h-2 w-6 border-b-2 border-solid border-brand-complementary')}/>
+            </div>
+          )
+          }
           </a>
         </div>
         <div className={classNames(
-          'navbar-menu',
+          ' w-full',
           {
             'is-active': isActive
           }
         )}
         >
-          <div className={classNames('navbar-end bg-blue-200')}>
-            <div className='navbar-item'>
+          <div className={classNames(' lg:flex justify-between flex-col lg:flex-row pt-4 lg:pt-0',{
+            'hidden': !isActive
+          })}>
+            <div className={classNames('w-full flex justify-center items-center flex-col lg:flex-row')}>
               {
                 navbarLinks.map(({ name, href, onlyLoggedIn }, index) => (
                   <Link href={href} key={index}>
                     <a className={classNames(
-                      'navbar-item',
+                      'py-4 lg:py-0 w-full lg:w-auto hover:text-brand-primary  hover:bg-brand-black-transparent lg:hover:bg-transparent lg:px-3 xl:px-4',
                       { 'is-hidden': onlyLoggedIn && !isLoggedIn }
                     )}
                     >
@@ -83,8 +102,8 @@ export default function Navbar() {
                 ))
               }
             </div>
-            <div className='btns'>
-              <div className='btn-sign-in'>
+            <div className={classNames('flex flex-col md:flex-row my-4 lg:my-0')}>
+              <div className={classNames('my-4 md:my-0 md:w-2/4 md:mr-5 lg:w-36 lg:mr-4 xl:mr-8 ')}>
                 {isLoggedIn &&
                   <Button
                     label='Cerrar sesión'
@@ -96,7 +115,7 @@ export default function Navbar() {
                     link='/login'
                   />}
               </div>
-              <div className='btn-apply'>
+              <div className={classNames(' md:w-2/4 md:ml-5 lg:ml-0 lg:w-36 lg:mr-0')}>
                 <Button
                   isPrimary
                   label='Aplica hoy'
