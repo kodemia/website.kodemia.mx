6395c9fdbbf2448fc0af9433ab7dece43c600098
charlesKodemia <charles@kodemia.mx>
2022-03-08 05:23:31 -0600

    ADD promo code to ApplyLandingForm

--- components/Forms/ApplyLandingForm.tsx
+++ components/Forms/ApplyLandingForm.tsx
@@ -17,133 +17,159 @@ import { useRouter } from 'next/router'
 
 type ApplyFormData = zod.infer<typeof schema>
 
-export default function ApplyLandingForm() {
-	const { register, handleSubmit, control, errors } = useForm<ApplyFormData>({
-		resolver: zodResolver(schema)
-	})
-	const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
-	const router = useRouter()
+type Props = {
+  includePromoCodeInput?: boolean
+  campaignName?: string
+}
 
-	const onSubmit = (data: ApplyFormData) => {
-		setIsSubmitting(true)
+const defaultProps: Props = {
+  includePromoCodeInput: true,
+  campaignName: 'landing'
+}
 
-		data.customFields = {
-			knowledge: data.knowledge,
-			reasonToProgramming: data.reasonToProgramming,
-			campaignName: 'landing'
-		}
+export default function ApplyLandingForm(props: Props = defaultProps) {
+  const { register, handleSubmit, control, errors } = useForm<ApplyFormData>({
+    resolver: zodResolver(schema)
+  })
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+  const router = useRouter()
 
-		apply.submit(data)
-			.then(() => router.push('aplicar/gracias/javascript-live'))
-			.catch(error => {
-				apply.errorHandler(error)
-				setIsSubmitting(false)
-			})
-			.finally(() => setIsSubmitting(false))
-	}
+  const onSubmit = (data: ApplyFormData) => {
+    setIsSubmitting(true)
+
+    data.customFields = {
+      knowledge: data.knowledge,
+      reasonToProgramming: data.reasonToProgramming,
+      campaignName: props.campaignName ?? 'landing'
+    }
+
+    apply.submit(data)
+      .then(() => router.push('aplicar/gracias/javascript-live'))
+      .catch(error => {
+        apply.errorHandler(error)
+        setIsSubmitting(false)
+      })
+      .finally(() => setIsSubmitting(false))
+  }
 
 
-	return (
-		<form
-			className='bg-gray-kd-dark border rounded-xl p-5'
-			onSubmit={handleSubmit(onSubmit)}
-		>
-			<ToastContainer position='top-center' />
-			<div className='pb-3'>
-				<Input
-					label='Nombre'
-					type='text'
-					placeholder='Nombre'
-					name='firstName'
-					required
-					register={register}
-					error={errors?.firstName?.message}
-				/>
-			</div>
+  return (
+    <form
+      className='bg-gray-kd-dark border rounded-xl p-5'
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <ToastContainer position='top-center' />
+      <div className='pb-3'>
+        <Input
+          label='Nombre'
+          type='text'
+          placeholder='Nombre'
+          name='firstName'
+          required
+          register={register}
+          error={errors?.firstName?.message}
+        />
+      </div>
 
-			<div className='pb-3'>
-				<Input
-					label='Apellido'
-					type='text'
-					placeholder='Apellido'
-					name='lastName'
-					required
-					register={register}
-					error={errors?.lastName?.message}
-				/>
-			</div>
+      <div className='pb-3'>
+        <Input
+          label='Apellido'
+          type='text'
+          placeholder='Apellido'
+          name='lastName'
+          required
+          register={register}
+          error={errors?.lastName?.message}
+        />
+      </div>
 
-			<div className='pb-3'>
-				<Input
-					label='Correo electrónico'
-					type='email'
-					placeholder='name@ejemplo.com'
-					name='email'
-					required
-					register={register}
-					error={errors?.email?.message}
-				/>
-			</div>
+      <div className='pb-3'>
+        <Input
+          label='Correo electrónico'
+          type='email'
+          placeholder='name@ejemplo.com'
+          name='email'
+          required
+          register={register}
+          error={errors?.email?.message}
+        />
+      </div>
 
-			<div className='pb-3'>
-				<Controller
-					name='phone'
-					control={control}
-					defaultValue='default'
-					render={({ value, onChange }) =>
-						<PhoneInput
-							label='Teléfono con Whatsapp'
-							error={errors?.phone?.message}
-							value={value}
-							onChange={onChange}
-						/>}
-				/>
-			</div>
+      <div className='pb-3'>
+        <Controller
+          name='phone'
+          control={control}
+          defaultValue='default'
+          render={({ value, onChange }) =>
+            <PhoneInput
+              label='Teléfono con Whatsapp'
+              error={errors?.phone?.message}
+              value={value}
+              onChange={onChange}
+            />}
+        />
+      </div>
 
-			<div className='pb-3'>
-				<Select
-					label='¿Tienes conocimientos previos en programación?'
-					register={register}
-					name='knowledge'
-					error={errors?.knowledge?.message}
-					options={[
-						{ value: 'Sí, soy/trabajo como programador' },
-						{ value: 'Sí, lo que vi en la universidad' },
-						{ label: 'No, vengo de cero', value: 'tomé algún curso' },
-						{ label: 'Sí, tomé algún curso', value: 'no, vengo de cero' }
-					]}
-					required
-				/>
-			</div>
+      <div className='pb-3'>
+        <Select
+          label='¿Tienes conocimientos previos en programación?'
+          register={register}
+          name='knowledge'
+          error={errors?.knowledge?.message}
+          options={[
+            { value: 'Sí, soy/trabajo como programador' },
+            { value: 'Sí, lo que vi en la universidad' },
+            { label: 'No, vengo de cero', value: 'tomé algún curso' },
+            { label: 'Sí, tomé algún curso', value: 'no, vengo de cero' }
+          ]}
+          required
+        />
+      </div>
 
-			<div className='pb-3'>
-				<Select
-					label='¿Por qué quieres aprender a programar?'
-					register={register}
-					name='reasonToProgramming'
-					error={errors?.reasonToProgramming?.message}
-					options={[
-						{ value: 'Quiero actualizarme' },
-						{ value: 'Para complementar mi educación' },
-						{ value: 'Para aplicarlo a mi emprendimiento' },
-						{ value: 'Complementar/cambiar mi carrera profesional' }
-					]}
-					required
-				/>
-			</div>
+      <div className='pb-3'>
+        <Select
+          label='¿Por qué quieres aprender a programar?'
+          register={register}
+          name='reasonToProgramming'
+          error={errors?.reasonToProgramming?.message}
+          options={[
+            { value: 'Quiero actualizarme' },
+            { value: 'Para complementar mi educación' },
+            { value: 'Para aplicarlo a mi emprendimiento' },
+            { value: 'Complementar/cambiar mi carrera profesional' }
+          ]}
+          required
+        />
+      </div>
 
-			<div className='pb-3'>
-				Al enviar este formulario estas aceptando nuestros <Link href='https://kodemia.mx/politica-de-privacidad'> Términos y condiciones </Link>
-			</div>
+      {
+        props.includePromoCodeInput === true &&
+        <div className='pb-3'>
+          <Input
+            label='Código de promoción'
+            type='text'
+            placeholder='Código de promoción'
+            name='promoCode'
+            required
+            register={register}
+            error={errors?.promoCode?.message}
+          />
+        </div>
+      }
 
-			<div className='pt-3'>
-				<Button
-					isPrimary
-					label={isSubmitting ? 'Enviando...' : 'Enviar'}
-					type='submit'
-					isDisabled={isSubmitting}
-				/>
-			</div>
-		</form>
-	)
+      <div className='pb-3'>
+        Al enviar este formulario estas aceptando nuestros <Link href='https://kodemia.mx/politica-de-privacidad'> Términos y condiciones </Link>
+      </div>
+
+
+      <div className='pt-3'>
+        <Button
+          isPrimary
+          label={isSubmitting ? 'Enviando...' : 'Enviar'}
+          type='submit'
+          isDisabled={isSubmitting}
+        />
+      </div>
+    </form>
+  )
 }
