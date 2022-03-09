c75c354aa5e632946c45b12d90d53c57d886d6f3
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2022-03-09 14:26:15 -0600

    :recycle: refactored Hero Js section and HeroJsInfo component

--- components/Pages/Bootcamps/Hero.tsx
+++ components/Pages/Bootcamps/Hero.tsx
@@ -23,51 +23,30 @@ export interface Props{
 
 export default function JavascriptLive ({ bootcamp, video }: Props): JSX.Element {
   return (
-    // <div className={classNames(
-    //   // 'hero-bootcamps'
-    // )}>
-      // <div className={classNames(
-      //   // 'columns hero-container'
-      // )}>
-        <div className={classNames(
-          // 'column is-full'
-          'h-screen',
-          'relative'
-        )}>
-          <BgVideo video={video} />
-          <PageSection
-            sectionClassName={classNames(
-              'absolute top-0',
-              'bg-gradient-to-t from-brand-black via-brand-black/60 to-brand-black/40',
-              'h-full',
-              // 'overflow-hidden',
-              'w-full'
-            )}
-            contentClassName='pt-20 md:pt-24 lg:pt-28'
-          >
-          {/* <div className={classNames(
-            // 'columns is-justify-content-center cover'
-          )}> */}
-            {/* <div className={classNames(
-              // 'column  container-card'
-            )}> */}
-              <HeroJsInfo bootcamp={bootcamp} />
-              <img
-                src='/icons/icon-scroll.svg'
-                className={classNames(
-                  // 'arrow-icon'
-                  'absolute left-0 bottom-0',
-                  'animate-bounce',
-                  'hidden md:block',
-                  'mx-[50%] md:mb-20',
-                  'w-7'
-                )}
-              />
-            {/* </div> */}
-          {/* </div> */}
-          </PageSection>
-        </div>
-      // </div>
-    // </div>
+    <div className='h-screen relative'>
+      <BgVideo video={video} />
+      <PageSection
+        sectionClassName={classNames(
+          'absolute top-0',
+          'bg-gradient-to-t from-brand-black via-brand-black/60 to-brand-black/40',
+          'h-full',
+          'overflow-hidden', // TODO: This is a desired behavior?
+          'w-full'
+        )}
+        contentClassName='pt-20 md:pt-24 lg:pt-28'
+      >
+        <HeroJsInfo bootcamp={bootcamp} />
+        <img
+          src='/icons/icon-scroll.svg'
+          className={classNames(
+            'absolute left-0 bottom-0',
+            'animate-bounce',
+            'hidden md:block',
+            'mx-[50%] md:mb-20',
+            'w-7'
+          )}
+        />
+      </PageSection>
+    </div>
   )
 }
