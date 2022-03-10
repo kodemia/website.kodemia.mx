09eaf5aa280bce173721523568b4c3894a7ceedb
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2022-03-09 23:09:07 -0600

    :lipstick: fix Hero content overflow

--- components/Pages/Bootcamps/Hero.tsx
+++ components/Pages/Bootcamps/Hero.tsx
@@ -23,26 +23,28 @@ export interface Props{
 
 export default function JavascriptLive ({ bootcamp, video }: Props): JSX.Element {
   return (
-    <div className='h-screen relative'>
+    <div className={classNames(
+      'h-[80vh]',
+      'relative'
+    )}>
       <BgVideo video={video} />
       <PageSection
         sectionClassName={classNames(
           'absolute top-0',
           'bg-gradient-to-t from-brand-black via-brand-black/60 to-brand-black/40',
           'h-full',
-          'overflow-hidden', // TODO: This is a desired behavior?
+          'overflow-hidden',
           'w-full'
         )}
-        contentClassName='pt-20 md:pt-24 lg:pt-28'
-      >
+        contentClassName='flex items-center'>
         <HeroJsInfo bootcamp={bootcamp} />
         <img
           src='/icons/icon-scroll.svg'
           className={classNames(
             'absolute left-0 bottom-0',
             'animate-bounce',
-            'hidden md:block',
-            'mx-[50%] md:mb-20',
+            'hidden sm:block',
+            'mx-[50%] sm:mb-14',
             'w-7'
           )}
         />
--- components/Pages/Bootcamps/HeroJsInfo.tsx
+++ components/Pages/Bootcamps/HeroJsInfo.tsx
@@ -23,8 +23,7 @@ export default function HeroJsInfo ({ bootcamp }: Props): JSX.Element {
   return (
     <div className='flex flex-col'>
       <div className={classNames(
-        'max-w-[205px]',
-        'w-full md:w-1/2 lg:w-1/3'
+        'w-full sm:w-1/2 lg:w-1/3'
       )}>
         <H5>
           {bootcamp.subtitle}
@@ -33,23 +32,24 @@ export default function HeroJsInfo ({ bootcamp }: Props): JSX.Element {
           {bootcamp.title}
         </H2>
       </div>
-      <div className='md:w-3/5 lg:w-1/3'>
         <p className={classNames(
-          'my-5 lg:my-8',
-          'max-w-[540px] md:max-w-[336px]',
+          'mt-8 mb-2',
+          'max-w-[540px] sm:max-w-[336px]',
           'text-brand-gray-light text-base font-medium'
         )}>
           {bootcamp.description}
         </p>
-      </div>
       <div className='w-full'>
         {
           bootcamp.information.map((data, index) => (
             <div
               key={`hero-js-info-${index}`}
+              className={classNames(
+
+              )}
             >
               <h6 className={classNames(
-                'mt-3',
+                'mt-2',
                 'text-brand-complementary text-[15px] md:text-lg font-semibold'
               )}>
                 {data.title}
