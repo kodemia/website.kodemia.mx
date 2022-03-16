abef427e7eb68907bf3c529d1a9922a015c1ea65
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2022-03-16 05:52:42 -0600

    :recycle: Investment section in Javascript page, refactor in progress

--- components/Pages/Bootcamps/Investment.tsx
+++ components/Pages/Bootcamps/Investment.tsx
@@ -1,8 +1,10 @@
 
 import React from 'react'
+import classNames from 'classnames'
 
 import H3 from 'components/H3'
 import H5 from 'components/H5'
+import PageSection from 'components/PageSection'
 import Text from 'components/Text'
 
 export interface Payment {
@@ -20,28 +22,68 @@ export interface Props {
   };
 }
 
-export default function Investment ({ investment }: Props) {
+export default function Investment ({ investment }: Props): JSX.Element {
   return (
-    <section className='columns investment '>
-      <div className='column is-full is-flex is-justify-content-center investment-container'>
-        <section className='columns is-multiline  investment-wrapper'>
-          <div className='column is-three-quarters investment-header'>
+    <PageSection
+      sectionClassName={classNames(
+        'bg-javascript-investment-lg',
+        'bg-cover bg-center bg-no-repeat',
+        'w-full'
+      )}
+      contentClassName={classNames(
+        ''
+      )}
+    >
+    {/* <section className={classNames(
+      // 'columns investment'
+      ''
+    )}> */}
+      <div className={classNames(
+        // 'column is-full is-flex is-justify-content-center',
+        // 'investment-container'
+        ''
+      )}>
+        <section className={classNames(
+          // 'columns is-multiline',
+          // 'investment-wrapper'
+          'flex-col',
+          'w-full' // TODO: Comprobar si es necesario este wrapper
+        )}>
+          <div className={classNames(
+            // 'column is-three-quarters',
+            // 'investment-header'
+            'w-3/4'
+          )}>
             <H5>
               {investment.subtitle}
             </H5>
-            <div className='investment-title'>
+            <div className={classNames(
+              // 'investment-title'
+              'max-w-[200px]'
+            )}>
               <H3
                 whiteText={investment.whiteTitle}
                 cyanText={investment.cyanTitle}
               />
             </div>
-            <div className='description'>
+            <div className={classNames(
+              // 'description'
+              'my-11', // TODO: Comprobar si es necesario este margin
+              'max-w-2xl' // TODO: Comprobar tamaño, en css está a 700px
+            )}>
               <Text>
                 {investment.description}
               </Text>
             </div>
           </div>
-          <div className='column is-full is-flex is-justify-content-space-between payment-types-container'>
+          <div className={classNames(
+            // 'column is-full is-flex is-justify-content-space-between',
+            // 'payment-types-container'
+            'flex flex-col md:flex-row',
+            'md:flex-wrap',
+            'justify-between',
+            'w-full'
+          )}>
             {
               investment.paymentTypes.map((paymentType, index) => (
                 <article
@@ -65,6 +107,7 @@ export default function Investment ({ investment }: Props) {
           </div>
         </section>
       </div>
-    </section>
+    {/* </section> */}
+    </PageSection>
   )
 }
