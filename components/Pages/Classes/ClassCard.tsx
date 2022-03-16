0104d3477ce8eec7a31339beb0e117a1fa606996
charlesKodemia <charles@kodemia.mx>
2022-03-16 07:41:52 -0600

    Migrate clases page to tailwind

--- components/Pages/Classes/ClassCard.tsx
+++ components/Pages/Classes/ClassCard.tsx
@@ -1,9 +1,11 @@
 
 import React from 'react'
-import Link from 'next/link'
 import dayjs from 'dayjs'
+import classNames from 'classnames'
+import router from 'next/router'
 
 export interface Props {
+  className: string
   klass: {
     date: string
     description: string
@@ -15,38 +17,65 @@ export interface Props {
   }
 }
 
-export default function ClassCard ({ klass }: Props) {
-  const id = klass.vimeoId
-  const isVimeo = !!klass.vimeoId
-  const title = klass.title
-  const date = dayjs(klass.date).format('DD/MM/YYYY')
+export default function ClassCard (props: Props) {
+  const id = props.klass.vimeoId
+  const isVimeo = !!props.klass.vimeoId
+  const title = props.klass.title
+  const date = dayjs(props.klass.date).format('DD/MM/YYYY')
 
   return (
-    <div className='class-card'>
-      <div className='poster-container'>
-        <img
-          src={klass.thumbnail}
-          className='poster'
+    <div
+      className={classNames(
+        'relative group hover:cursor-pointer',
+        props.className
+      )}
+      onClick={() => {
+        // TODO: change clase page to use uri params instead of query (/clases/[id])
+        router.push({
+          pathname: '/clase',
+          query: { id, title, isVimeo }
+        })
+      }}
+    >
+      <img
+        src={props.klass.thumbnail}
+        className='h-auto object-contain mx-auto'
+      />
+      <div className={classNames(
+          'absolute h-auto w-full',
+          'bg-brand-black-dark/50 group-hover:bg-brand-black-dark/20',
+          'flex justify-center items-center',
+          'top-0 left-0',
+      )}>
+        <img // image duplicated to save the proportions on the overlay
+          src={props.klass.thumbnail}
+          className=' h-auto object-contain invisible'
         />
+        <figure className={classNames(
+          'absolute',
+          'h-14 w-14',
+          'bg-[url(/icons/icon-video.svg)] group-hover:bg-[url(/icons/icon-video-blue.svg)]',
+        )}/>
       </div>
-      <Link href={{ pathname: '/clase', query: { id, title, isVimeo } }}>
-        <div className='icon-container'>
-          <img
-            src='/icons/icon-video.svg'
-            alt='play-class-bootcamp'
-            className='icon-play'
-          />
-          <img
-            src='/icons/icon-video-blue.svg'
-            alt='play-class-bootcamp'
-            className='icon-play-blue'
-          />
-        </div>
-      </Link>
-      <div className='data-container is-flex is-flex-direction-column is-justify-content-center'>
-        <h6 className='class-title'>{klass.title}</h6>
-        <p className='class-date'>Fecha: {date}</p>
+
+      <div className='pt-3 text-center md:text-left'>
+        <p className='text-brand-h5 pb-2 text-brand-complementary'>
+          {props.klass.title}
+        </p>
+        <p className='text-brand-gray-light'>
+          Fecha: {date}
+        </p>
       </div>
+    </div>
+  )
+}
+
+export function ClassCardLoading ({ className }: { className?: string }): JSX.Element {
+  return (
+    <div className={classNames('animate-pulse', className)}>
+      <figure className={`h-44 w-full bg-brand-black`} />
+      <p className={`h-6 w-full mt-4 bg-brand-black`} />
+      <p className={`h-4 w-1/2 mt-4 bg-brand-black`} />
     </div>
   )
 }
--- pages/clases.tsx
+++ pages/clases.tsx
@@ -2,15 +2,16 @@
 import React, { useState, useEffect } from 'react'
 import { ToastContainer } from 'react-toastify'
 
-import ClassCard from 'components/Pages/Classes/ClassCard'
+import ClassCard, { ClassCardLoading } from 'components/Pages/Classes/ClassCard'
 import Footer from 'components/Footer'
-import H3 from 'components/H3'
-import H5 from 'components/H5'
 import Navbar from 'components/Navbar'
+import PageSection from 'components/PageSection'
+import PageHero from 'components/PageHero'
 import Seo from 'components/SEO'
 
 import { useAuth } from 'lib/hooks'
 import classesService from 'lib/api/classes'
+import _ from 'lodash'
 
 export interface Class {
   date: string
@@ -32,46 +33,45 @@ export default function Classes () {
   }, [])
 
   return (
-    <div className='classes'>
-      <Seo />
+    // <div className='classes'>
+    <>
+      <Seo
+        title='Kodemia | clases'
+        description='¿Quieres estudiar? Aquí puedes volver a ver tus clases pasadas'
+      />
       <Navbar />
       <ToastContainer position='top-center' />
-      <div className='columns is-multiline classes-container'>
-        <div className='column is-full classes-bg'>
-          <div className='title-container is-flex is-justify-content-center'>
-            <div className='wrapper is-flex is-flex-direction-column is-justify-content-center'>
-              <H5>
-                Clases Kodemia
-              </H5>
-              <div className='h2'>
-                <H3
-                  whiteText='No te pierdas'
-                  cyanText='ninguna'
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='column is-flex is-justify-content-center classes-wrapper'>
-          <div className='columns is-multiline  classes-cards'>
-            {classes.length === 0 &&
-              <div className='column'>
-                <progress className='progress is-small is-info' max='100'>
-                  15%
-                </progress>
-              </div>}
-            {classes.map((klass, index) =>
-              <div
-                key={`class-${index}`}
-                className='column is-4-desktop is-6-tablet is-flex is-justify-content-center'
-              >
-                <ClassCard klass={klass} />
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
+
+      <PageHero
+        subTitle='Clases kodemia'
+        title='No te pierdas ninguna'
+        hasSeparatedLines
+        headerClassName={'bg-clases-hero-sm md:bg-clases-hero-md lg:bg-clases-hero-lg'}
+      />
+
+      <PageSection
+        sectionClassName='min-h-[50vh]'
+        contentClassName={'flex flex-row flex-wrap justify-between'}
+      >
+        {/* Loading animation */}
+        {_.isEmpty(classes) && _.range(6).map((_, index) => (
+            <ClassCardLoading
+              key={`klass-placeholder-${index}`}
+              className='mb-8 grow md:grow-0 basis-full md:basis-[48%] lg:basis-[32%]'
+            />
+        ))}
+
+        {/* Class list */}
+        {!_.isEmpty(classes) && classes.map((klass, index) => (
+          <ClassCard
+            key={`class-card-${index}`}
+            className='mb-8 grow md:grow-0 basis-full md:basis-[48%] lg:basis-[32%]'
+            klass={klass}
+          />
+        ))}
+      </PageSection>
+
       <Footer />
-    </div>
+    </>
   )
 }
