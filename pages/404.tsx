edaa6a5e1ae5f66ddc4b6b84030a5c5301b7c6dc
arianaomi <arianaomi.lp@gmail.com>
2022-03-17 00:07:22 -0600

    Updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,44 +1,34 @@
 
 import Button from 'components/Button'
 import Footer from 'components/Footer'
+import H1 from 'components/H1'
 import H5 from 'components/H5'
 import Navbar from 'components/Navbar'
+import BigMessagePage from 'components/BigMessagePage'
 
-export default function Custom404 () {
+export default function Custom404() {
   return (
     <>
       <Navbar />
-      <section className='hero has-background-black-bis is-fullheight' id='custom-404'>
-        <div className='hero-body'>
-          <div className='container has-text-centered'>
-            <div className='columns is-centered'>
-              <div className='column is-half'>
-                <div className='card has-background-grey-darker p-5 is-rounded'>
-                  <div className='columns is-multiline container'>
-                    <div className='column is-full mt-5'>
-                      <p className='is-size-1 has-text-weight-bold has-text-white'>
-                        404
-                      </p>
-                      <div className='column is-full mt-5 mb-5'>
-                        <H5>
-                          Esta página no fue encontrada 🤕
-                        </H5>
-                      </div>
-                      <div className='column is-full mb-5'>
-                        <Button
-                          isPrimary
-                          label='Regresar a Home'
-                          link='/'
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <BigMessagePage >
+        <div className='text-center'>
+          <H1>
+            404
+          </H1>
+          <div className='my-12 w-64'>
+            <H5>
+              Esta página no fue encontrada 🤕
+            </H5>
+          </div>
+          <div className='w-64'>
+            <Button
+              isPrimary
+              label='Regresar a Home'
+              link='/'
+            />
           </div>
         </div>
-      </section>
+      </BigMessagePage>
       <Footer />
     </>
   )
