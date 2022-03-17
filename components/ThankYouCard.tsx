355316c04c9979cb2310508e50c1b60f93920508
arianaomi <arianaomi.lp@gmail.com>
2022-03-16 20:40:50 -0600

    remove footer

--- components/ThankYouCard.tsx
+++ components/ThankYouCard.tsx
@@ -15,36 +15,34 @@ export default function ThankyouCard({ children, text }: Props): JSX.Element {
   const description = !text ? 'Te contactaremos lo más \n pronto posible' : text
 
   return (
-    <>
-      <PageSection
-        sectionClassName='bg-brand-black h-screen'
-        contentClassName=' my-auto'
-      >
-        <div className={classNames(
-          'bg-brand-black-light',
-          'flex flex-col items-center justify-center',
-          'rounded-wide',
-          'px-6 py-9 sm:mx-auto',
-          'sm:w-2/3 lg:w-2/4',
-        )}>
-          <div className=' mb-12 text-center'>
-            <H3>
-              <span className='text-brand-primary'>¡Muchas gracias</span> por
-              <br />
-              registrarte<Cursor white>!</Cursor>
-            </H3>
-          </div>
-          <div className='text-brand-gray-light text-center sm:w-80 whitespace-pre-line'>
-            {description}
-          </div>
-          {children &&
-            <div className='mt-12'>
-              {children}
-            </div>
-          }
+
+    <PageSection
+      sectionClassName='bg-brand-black h-screen'
+      contentClassName=' my-auto'
+    >
+      <div className={classNames(
+        'bg-brand-black-light',
+        'flex flex-col items-center justify-center',
+        'rounded-wide',
+        'px-6 py-9 sm:mx-auto',
+        'sm:w-2/3 lg:w-2/4',
+      )}>
+        <div className=' mb-12 text-center'>
+          <H3>
+            <span className='text-brand-primary'>¡Muchas gracias</span> por
+            <br />
+            registrarte<Cursor white>!</Cursor>
+          </H3>
         </div>
-      </PageSection>
-      <Footer />
-    </>
+        <div className='text-brand-gray-light text-center whitespace-pre-line sm:w-80'>
+          {description}
+        </div>
+        {children &&
+          <div className='mt-12'>
+            {children}
+          </div>
+        }
+      </div>
+    </PageSection>
   )
 }
